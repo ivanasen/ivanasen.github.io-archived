48e7329d8c304681dcf5fc1c57f2bb92f680e741
vankata9907 <vankata9907@gmail.com>
2017-06-25 22:02:37 +0300

    made greeting screen

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
 $(document).ready(() => {
     $(".button-collapse").sideNav();
     $(".parallax").parallax();
-})
+});
+
+$(window).scroll(function () {
+    $(".top").css("opacity", 1 - $(window).scrollTop() / 800);
+});
+
+$('.btn-scroll-to-about-me').click(() => {
+    $('html, body').animate({
+        scrollTop: $("#about-me").offset().top
+    }, 1000);
+});
+
+$('.btn-scroll-to-projects').click(() => {
+    $('html, body').animate({
+        scrollTop: $("#projects").offset().top
+    }, 1000);
+});
