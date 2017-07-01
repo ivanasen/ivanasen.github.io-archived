3a8da31271fbc800e34b2bd88d07ad9a12a491fd
vankata9907 <vankata9907@gmail.com>
2017-07-01 17:30:14 +0300

    fixed navbar bugs

--- js/main.js
+++ js/main.js
@@ -1,17 +1,28 @@
+const navBackgroundAnimationSpeed = 700;
+const navSecondaryAnimationSpeed = 300;
+
+let animateNavbar = (scroll) => {
+    $(".top").css("opacity", 1 - scroll / navBackgroundAnimationSpeed);
+    $("nav").css("cssText",
+        `box-shadow: 0 2px 2px rgba(0, 0, 0, ${Math.min(.2, scroll / navSecondaryAnimationSpeed )}) !important;
+        background-color: rgba(255, 255, 255, ${(scroll / navSecondaryAnimationSpeed) - 1})`
+    );
+
+    const navbarButtonColors = {
+        red: Math.max(255 - scroll / 2, 33),
+        green: Math.max(255 - scroll / 5, 150),
+        blue: Math.max(255 - scroll / 10, 240)
+    };
+    $(".navbar-fixed nav li .menu-btn")
+        .css("color", `rgb(${navbarButtonColors.red}, ${navbarButtonColors.green}, ${navbarButtonColors.blue})`);
+}
+
 $(document).ready(() => {
     $(".button-collapse").sideNav();
     $(".parallax").parallax();
 });
 
-$(window).scroll(function () {
-    $(".top").css("opacity", 1 - $(window).scrollTop() / 700);
-    $("nav").css("background-color", `rgba(255, 255, 255, ${($(window).scrollTop() / 300) - 1})`);
-
-    let red = Math.max(255 - $(window).scrollTop() / 2, 33);
-    let green = Math.max(255 - $(window).scrollTop() / 5, 150);
-    let blue = Math.max(255 - $(window).scrollTop() / 10, 240);
-    $(".navbar-fixed nav li .menu-btn").css("color", `rgb(${red}, ${green}, ${blue})`);
-});
+$(window).scroll(() => animateNavbar($(window).scrollTop()));
 
 $('.btn-scroll-about-me').click(() => {
     $('html, body').animate({
